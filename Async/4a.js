71b39fc3b6dbd9ea6a7faef25dc3b50c78925f80
Sriram Pendyala <sreerampendyala96@gmail.com>
2018-12-24 15:57:31 +0530

    Images Downloading

--- Async/4a.js
+++ Async/4a.js
@@ -2,17 +2,33 @@ var fs = require('fs');
 var imgUri = require('./Imageuri.js');
 var request = require('request');
 var async = require('async');
-var pathFront = './Images';
+var dir= './Images';
+var pathFront;
 var pathExtention ='.jpg';
 
-async.parallel(imgUri.imageUri(function(err,imglog){
+function newDir(int=0){
+  if(!fs.existsSync(dir+int)){
+    fs.mkdirSync(dir+int);
+    pathFront = dir+int+'/Image';
+
+  }
+  else pathFront = dir+int+'/Image';  
+}
+
+function pathset(key){
+  console.log(key)
+  var path= parseInt(key/5);
+  newDir(path);
+  console.log(pathFront+key+pathExtention);
+  return pathFront+key+pathExtention;
+}
+
+async.series(imgUri.imageUri(function(err,imglog){
   async.forEachOf(imglog,function(uri,key,callback){
-    k=1;
-    console.log(imglog.indexOf(uri));
-    request({uri,encoding: 'binary'},function(error,response,body){
-      fs.writeFile(pathFront+k+pathExtention,body,'binary',function(err){
+        request({uri,encoding: 'binary'},function(error,response,body){
+            //console.log(pathset(key));
+          fs.writeFile(pathset(key),body,'binary',function(err){
         if (err) console.log("err");
-        k++;
       })
     })
     callback();
