0d566902ccb50166ba58d554f32322f269c17ed6
Sriram Pendyala <sreerampendyala96@gmail.com>
2018-12-27 13:00:59 +0530

    ShortenUrl

--- ShorterUrl/Imageuri.js
+++ ShorterUrl/Imageuri.js
@@ -2,15 +2,15 @@
 
   var imageUri = function (callback) {
   var uri=[];
+  uri.push('https://static.vecteezy.com/system/resources/previews/000/107/464/non_2x/huge-doodle-vector-pack.jpg');
+  uri.push('https://media.glassdoor.com/l/e9/c1/7a/84/independence-day-celebration.jpg');
+  uri.push('https://i.ytimg.com/vi/O5u1apUkYV0/maxresdefault.jpg');
   uri.push('http://sousmonarbre.com/qphj/bd963843d2239ed78aa6f7b0a36b537d/qdp/shapely-table-mat-design-office-bay-decoration-mes-at-work-decorating-ideas-office-decoration-mes-design-ideas-cream-wall-paint-decoration-messroom-wooden-laminate-ing-tosca-color__office-decorating-ideas.jpg');
   uri.push('https://doodleart.redbull.com/assets/managed/entries/processed/sm/367010617181759_36211000.jpg');
   uri.push('https://www.justcolor.net/wp-content/uploads/sites/1/nggallery/doodle-art-doodling/coloring-page-adults-doodle-art-rachel.jpg');
   uri.push('https://i.pinimg.com/originals/e5/55/a3/e555a39ca5457a079a9bcce59f61f8d5.jpg');
   uri.push('http://canhotopazelite.info/wp-content/uploads/2018/08/office-bay-decoration-themes-with-office-bay-decoration-themes-elegant-yet-fun-office-bay-decoration-14.jpg');
   uri.push('https://i.pinimg.com/originals/ef/4c/91/ef4c91fb73e61e19211a0589187ccaa6.jpg');
-  uri.push('https://static.vecteezy.com/system/resources/previews/000/107/464/non_2x/huge-doodle-vector-pack.jpg');
-  uri.push('https://i.ytimg.com/vi/O5u1apUkYV0/maxresdefault.jpg');
-  uri.push('https://media.glassdoor.com/l/e9/c1/7a/84/independence-day-celebration.jpg');
   callback(null,uri);
 };
 module.exports.imageUri=imageUri;
--- ShorterUrl/shorterUrl.js
+++ ShorterUrl/shorterUrl.js
@@ -1,20 +1,22 @@
 var uriArr = require('./imageUri');
 var shortUrl = require('node-url-shortener');
+var async =require('async');
 var csv = require('node-csv').createParser();
 
 
 function shortenUrl(){
-  var arr=[];
+  var arr=[], k=0;
   uriArr.imageUri(function(err,imgLog){
-    for(var i in imgLog){
-      shortUrl.short(imgLog[i], function(err, url){
-        var str = imgLog[i]+","+url+"\n"
+    async.series(async.forEachOf(imgLog, function(value,key,callback){
+      shortUrl.short(value, function(err, url){
+        var str = imgLog[k]+","+url+"\n"
+        k++;
         csv.parse(str,function(err,data){
           console.log(data);
         })
       });
-    }
+      callback();
+    }))
   })
 }
-
 shortenUrl();
